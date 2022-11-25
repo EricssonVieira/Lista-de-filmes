ff7239cede0b7a434ae1b3ec6187ed9b7b11c3a7
EricssonVieira <ericssonrvieira@gmail.com>
2022-11-25 18:06:58 -0300

    ajuste na seleçao do filme pronta

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const vote_average = document.querySelector('#vote_average')
 const date = document.querySelector('#date')
 const banner = document.querySelector('.banner')
 const carrosel = document.querySelector('.carrosel')
+const carrosel_ID = document.getElementById('carrosel')
 
 const getMovies = async movie => {
   const apiMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=cb7052f34586626bf335bdee04865a56`
@@ -15,25 +16,42 @@ const getMovies = async movie => {
 
 // ====================================================================================
 
-const showMovies = async movie => {
-  const data = await getMovies(movie)
-  const indexMovie = movie
-  const topRated = data.results
-  const path = topRated.map(movies => movies.poster_path)[indexMovie]
-  var url = `https://image.tmdb.org/t/p/w500${path}`
-  const title = topRated.map(movies => movies.title)[indexMovie]
+  
+// essa serve pra achar o id do filme  
+const li = document.getElementById("carrosel_ID");
 
-  title_movies.innerText = title
-  description.innerText = topRated.map(movies => movies.overview)[indexMovie]
-  vote_average.innerText =
-    topRated.map(movies => movies.vote_average)[indexMovie] * 10 +
+  li.addEventListener("click", function(event)  {
+    const target = event.target
+    const index = target.className
+
+    showMovies(index)
+  })
+  
+  showMovies = async (index) => {
+    const data = await getMovies(index)
+    const topRated = data.results
+    const path = topRated.map(movies => movies.poster_path)[index]
+    const url = `https://image.tmdb.org/t/p/w500${path}`
+    const title = topRated.map(movies => movies.title)[index]
+    
+    
+    title_movies.innerText = title
+    description.innerText = topRated.map(movies => movies.overview)[index]
+    vote_average.innerText =
+    topRated.map(movies => movies.vote_average)[index] * 10 +
     '% de aprovação'
-  date.innerText = topRated.map(movies => movies.release_date)[indexMovie]
-  banner.style.backgroundImage = `url(${url})`
-}
-showMovies(10)
+    date.innerText = topRated.map(movies => movies.release_date)[index]
+    banner.style.backgroundImage = `url(${url})`
+    
+    
+    window.scrollTo({
+      top:0,
+      behavior: "smooth"
 
-// ====================================================================================
+    });
+      }
+      
+// =========================================================================
 
 function modal() {
   var element = document.getElementById('modal-overlay')
@@ -77,8 +95,8 @@ function addFilme() {
 
 function updateUI() {
   var values = [],
-    keys = Object.keys(localStorage),
-    i = keys.length
+  keys = Object.keys(localStorage),
+  i = keys.length
   while (i--) {
     values.push(localStorage.getItem(keys[i]))
   }
@@ -86,9 +104,8 @@ function updateUI() {
 
 // =================================================================================
 
-// ---------------------------------- AINDA PRECISO ARRUMAR ISSO --------------------------------
-
-const showMoviess = async () => {
+// essa funçao serve pra mostrar todos os mini banner vindo da api
+const catalogo = async () => {
   const apiMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=cb7052f34586626bf335bdee04865a56`
 
   const res = await fetch(apiMoviesURL)
@@ -99,6 +116,8 @@ const showMoviess = async () => {
   for (i = 0; i < cont; i++) {
     const path = topRated.map(movies => movies.poster_path)[i]
     const url = `https://image.tmdb.org/t/p/w500${path}`
+    const id = i
+    
 
     const div = document.createElement('div')
     const img = document.createElement('img')
@@ -110,16 +129,16 @@ const showMoviess = async () => {
     
     div.appendChild(img)
     div.appendChild(link)
+
     div.classList.add('mini-banner')
+    img.classList.add(i)
     
     carrosel.appendChild(div)
   }
 }
-showMoviess()
+catalogo()
 
 // =========================================================================
 
-
-// =========================================================================
 
 //
