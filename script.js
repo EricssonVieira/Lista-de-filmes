de8cce431b147ad5c9b3f8af363ddfa1590bae76
EricssonVieira <ericssonrvieira@gmail.com>
2022-11-27 19:04:35 -0300

    ajustando a lista de filmes

--- script.js
+++ script.js
@@ -19,44 +19,39 @@ const getMovies = async movie => {
 
 // ====================================================================================
 
-  
-// essa serve pra achar o id do filme  
-const li = document.getElementById("carrosel_ID");
+// essa serve pra achar o id do filme
+const li = document.getElementById('carrosel_ID')
 
-  li.addEventListener("click", function(event)  {
-    const target = event.target
-    const index = target.className
+li.addEventListener('click', function (event) {
+  const target = event.target
+  const index = target.className
 
-    showMovies(index)
+  showMovies(index)
+})
+
+showMovies = async index => {
+  const data = await getMovies(index)
+  const topRated = data.results
+  const path = topRated.map(movies => movies.poster_path)[index]
+  const url = `https://image.tmdb.org/t/p/w500${path}`
+  const title = topRated.map(movies => movies.title)[index]
+
+  title_movies.innerText = title
+  description.innerText = topRated.map(movies => movies.overview)[index]
+  vote_average.innerText =
+    topRated.map(movies => movies.vote_average)[index] * 10 + '% de aprovação'
+  date.innerText = topRated.map(movies => movies.release_date)[index]
+  banner.style.backgroundImage = `url(${url})`
+  banner.style.width = '330px'
+  banner.style.height = '420px'
+  text_main.style.display = 'none'
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   })
-  
-  showMovies = async (index) => {
-    const data = await getMovies(index)
-    const topRated = data.results
-    const path = topRated.map(movies => movies.poster_path)[index]
-    const url = `https://image.tmdb.org/t/p/w500${path}`
-    const title = topRated.map(movies => movies.title)[index]
-    
-    
-    title_movies.innerText = title
-    description.innerText = topRated.map(movies => movies.overview)[index]
-    vote_average.innerText =
-    topRated.map(movies => movies.vote_average)[index] * 10 +
-    '% de aprovação'
-    date.innerText = topRated.map(movies => movies.release_date)[index]
-    banner.style.backgroundImage = `url(${url})`
-    banner.style.width = "330px"
-    banner.style.height = "420px"
-    text_main.style.display = "none"
-    
-    
-    window.scrollTo({
-      top:0,
-      behavior: "smooth"
+}
 
-    });
-      }
-      
 // =========================================================================
 
 function modal() {
@@ -70,9 +65,8 @@ listModal = () => {
 }
 // ======================================================================
 
-function contar() {
-  const count = localStorage
-  return count.length
+contar = () => {
+  return localStorage.length
 }
 
 // ======================================================================
@@ -80,37 +74,29 @@ function contar() {
 addFilme = () => {
   contar()
   const title = document.querySelector('#title')
-const resume = document.querySelector('#resume')
-const nota = document.querySelector('#nota')
-const data = document.querySelector('#data')
-const file = document.querySelector('#file')
+  const resume = document.querySelector('#resume')
+  const nota = document.querySelector('#nota')
+  const data = document.querySelector('#data')
+  const file = document.querySelector('#file')
 
-const teste = localStorage.setItem(
-  title.value ,
-  JSON.stringify({
-    title: title.value,
-    resume: resume.value,
-    nota: nota.value,
-    data: data.value,
-    file: file.value,
-    id: contar()
-  })
+  const teste = localStorage.setItem(
+    title.value,
+    JSON.stringify({
+      title: title.value,
+      resume: resume.value,
+      nota: nota.value,
+      data: data.value,
+      file: file.value,
+      id: contar()
+    })
   )
   movies.push(teste)
-  
-  updateUI()
+
+  modal()
+  window.location.reload()
 }
 
 // =================================================================================
-
-function updateUI() {
-  var values = [],
-  keys = Object.keys(localStorage),
-  i = keys.length
-  while (i--) {
-    values.push(localStorage.getItem(keys[i]))
-  }
-}
 
 // =================================================================================
 
@@ -120,11 +106,9 @@ const teste = async () => {
   const res = await fetch(apiMoviesURL)
   const data = await res.json()
   const topRated = data.results
-  const cont = topRated.length
+  const qtd = 18
 
-
-  
-  for (i = 0; i < 20; i++) {
+  for (i = 0; i < qtd; i++) {
     const id = topRated.map(movies => movies.id)[i]
     const title = topRated.map(movies => movies.title)[i]
     const release_date = topRated.map(movies => movies.release_date)[i]
@@ -143,12 +127,12 @@ const teste = async () => {
         title: title,
         id: id
       })
-      )
-      movies.push(teste)
-    }
+    )
+    movies.push(teste)
   }
-  
-  teste()
+}
+
+teste()
 
 // essa funçao serve pra mostrar todos os mini banner vindo da api
 const catalogo = async () => {
@@ -159,13 +143,10 @@ const catalogo = async () => {
   const topRated = data.results
   const cont = topRated.length
 
- 
-
   for (i = 0; i < cont; i++) {
     const path = topRated.map(movies => movies.poster_path)[i]
     const url = `https://image.tmdb.org/t/p/w500${path}`
     const id = i
-    
 
     const div = document.createElement('div')
     const img = document.createElement('img')
@@ -173,36 +154,51 @@ const catalogo = async () => {
 
     img.setAttribute('src', url)
 
-    link.textContent = "Ver mais"
-    
+    link.textContent = 'Ver mais'
+
     div.appendChild(img)
     div.appendChild(link)
 
     div.classList.add('mini-banner')
     img.classList.add(i)
-    
+
     carrosel.appendChild(div)
   }
 }
-
 catalogo()
 
 // =========================================================================
 
 getItens = () => {
-    const list = document.querySelector('.list')
-    const keys = Object.keys(localStorage);
-    // console.log(keys);
+  const list = document.querySelector('.list')
+  const keys = Object.keys(localStorage)
 
-    for (const key of keys) {
-      const filme = document.createElement('p');
+  for (const key of keys) {
+    const filme = document.createElement('p')
+    const div = document.createElement('div')
+    const btnEditar = document.createElement('img')
+    btnEditar.setAttribute('src', '../img/botao-editar.png')
+    const btnExcluir = document.createElement('img')
+    btnExcluir.setAttribute('src', '../img/botao-x.png')
 
-      filme.innerText = key
-      list.appendChild(filme)
-      list.classList.add('lista')
+    filme.innerText = key
 
-      console.log(key)
-    
-}}
+    btnExcluir.classList.add('btn-excluir')
+    btnEditar.classList.add('btn-editar')
+    div.classList.add('editor')
+
+    filme.appendChild(div)
+    list.appendChild(filme)
+    div.appendChild(btnEditar)
+    div.appendChild(btnExcluir)
+    list.classList.add('lista')
+  }
+}
 
 getItens()
+
+const btnEditar = document.querySelector('.editar')
+const element = document.querySelectorAll('.editor')
+element.forEach(element => {
+  btnEditar.addEventListener('click', () => element.classList.toggle('active'))
+})
