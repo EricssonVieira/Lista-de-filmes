5deebb140a46ef2415eb51554cc3c9e8e487f3c0
EricssonVieira <ericssonrvieira@gmail.com>
2022-11-28 20:52:05 -0300

    dados carregados do localStorage e nao mais da api

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ const banner = document.querySelector('.banner')
 const text_main = document.querySelector('.text-main')
 const carrosel = document.querySelector('.carrosel')
 const carrosel_ID = document.getElementById('carrosel')
-
+const li = document.getElementById('carrosel_ID')
+const qtd = localStorage.length
 const movies = localStorage.getItem('movies') || []
+
+
+voltarTopo = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
 
 const getMovies = async movie => {
   const apiMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=cb7052f34586626bf335bdee04865a56`
@@ -20,57 +29,53 @@ const getMovies = async movie => {
 // ====================================================================================
 
 // essa serve pra achar o id do filme
-const li = document.getElementById('carrosel_ID')
 
 li.addEventListener('click', function (event) {
   const target = event.target
   const index = target.className
 
-  showMovies(index)
+  for ( let i = 0; i < qtd; i++ ) {
+
+    let key = localStorage.key(i);
+    let valor = JSON.parse(localStorage.getItem(key));
+
+    if(index == valor.id){
+      title_movies.innerText = valor.title
+      description.innerText = valor.resume
+      vote_average.innerText = valor.nota
+      date.innerText = valor.data
+      let url = valor.file
+
+      banner.style.backgroundImage = `url(${url})`
+      banner.style.width = '330px'
+      banner.style.height = '420px'
+      text_main.style.display = 'none'
+
+    }
+  }
+  voltarTopo();
 })
-
-showMovies = async index => {
-  const data = await getMovies(index)
-  const topRated = data.results
-  const path = topRated.map(movies => movies.poster_path)[index]
-  const url = `https://image.tmdb.org/t/p/w500${path}`
-  const title = topRated.map(movies => movies.title)[index]
-
-  title_movies.innerText = title
-  description.innerText = topRated.map(movies => movies.overview)[index]
-  vote_average.innerText =
-    topRated.map(movies => movies.vote_average)[index] * 10 + '% de aprovação'
-  date.innerText = topRated.map(movies => movies.release_date)[index]
-  banner.style.backgroundImage = `url(${url})`
-  banner.style.width = '330px'
-  banner.style.height = '420px'
-  text_main.style.display = 'none'
-
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  })
-}
 
 // =========================================================================
 
-function modal() {
+modal = () => {
   var element = document.getElementById('modal-overlay')
   element.classList.toggle('active')
+  voltarTopo();
 }
 // ======================================================================
 listModal = () => {
   var element = document.getElementById('modal-overlay-list')
   element.classList.toggle('active')
+  
+  voltarTopo();
 }
 // ======================================================================
-
 contar = () => {
+
   return localStorage.length
 }
-
 // ======================================================================
-
 addFilme = () => {
   contar()
   const title = document.querySelector('#title')
@@ -78,7 +83,7 @@ addFilme = () => {
   const nota = document.querySelector('#nota')
   const data = document.querySelector('#data')
   const file = document.querySelector('#file')
-
+  
   const teste = localStorage.setItem(
     title.value,
     JSON.stringify({
@@ -89,64 +94,63 @@ addFilme = () => {
       file: file.value,
       id: contar()
     })
-  )
-  movies.push(teste)
-
-  modal()
-  window.location.reload()
-}
-
-// =================================================================================
-
-// =================================================================================
-
-const teste = async () => {
-  const apiMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=cb7052f34586626bf335bdee04865a56`
-
-  const res = await fetch(apiMoviesURL)
-  const data = await res.json()
-  const topRated = data.results
-  const qtd = 18
-
-  for (i = 0; i < qtd; i++) {
-    const id = topRated.map(movies => movies.id)[i]
-    const title = topRated.map(movies => movies.title)[i]
-    const release_date = topRated.map(movies => movies.release_date)[i]
-    const vote_average = topRated.map(movies => movies.vote_average)[i]
-    const resume = topRated.map(movies => movies.overview)[i]
-    const path = topRated.map(movies => movies.poster_path)[i]
-    const url = `https://image.tmdb.org/t/p/w500${path}`
-
-    const teste = localStorage.setItem(
-      title,
-      JSON.stringify({
-        resume: resume,
-        nota: vote_average,
-        data: release_date,
-        file: url,
-        title: title,
-        id: id
-      })
     )
     movies.push(teste)
-  }
+    
+    modal()
+    window.location.reload()
 }
+// ======================================================================
+  
 
-teste()
+
+// Ao descomentar essa funçao, ela preenche o localStorage com filmes
+// vindo da api TMDB
+
+// const apiMovies = async () => {
+//   const apiMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=cb7052f34586626bf335bdee04865a56`
+
+//   const res = await fetch(apiMoviesURL)
+//   const data = await res.json()
+//   const topRated = data.results
+//   const qtd = 18
+
+//   for (i = 0; i < qtd; i++) {
+//     const id = topRated.map(movies => movies.id)[i]
+//     const title = topRated.map(movies => movies.title)[i]
+//     const release_date = topRated.map(movies => movies.release_date)[i]
+//     const vote_average = topRated.map(movies => movies.vote_average)[i]
+//     const resume = topRated.map(movies => movies.overview)[i]
+//     const path = topRated.map(movies => movies.poster_path)[i]
+//     const url = `https://image.tmdb.org/t/p/w500${path}`
+
+//     const movie = localStorage.setItem(
+//       title,
+//       JSON.stringify({
+//         resume: resume,
+//         nota: vote_average,
+//         data: release_date,
+//         file: url,
+//         title: title,
+//         id: id
+//       })
+//     )
+//     movies.push(movie)
+//   }
+// }
+
+// apiMovies()
 
 // essa funçao serve pra mostrar todos os mini banner vindo da api
+
 const catalogo = async () => {
-  const apiMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=cb7052f34586626bf335bdee04865a56`
 
-  const res = await fetch(apiMoviesURL)
-  const data = await res.json()
-  const topRated = data.results
-  const cont = topRated.length
-
-  for (i = 0; i < cont; i++) {
-    const path = topRated.map(movies => movies.poster_path)[i]
-    const url = `https://image.tmdb.org/t/p/w500${path}`
-    const id = i
+  for ( let i = 0; i < qtd; i++ ) {
+    let key = localStorage.key(i);
+    let valor = JSON.parse(localStorage.getItem(key));
+  
+    const url = valor.file
+    const id = valor.id
 
     const div = document.createElement('div')
     const img = document.createElement('img')
@@ -160,7 +164,7 @@ const catalogo = async () => {
     div.appendChild(link)
 
     div.classList.add('mini-banner')
-    img.classList.add(i)
+    img.classList.add(id)
 
     carrosel.appendChild(div)
   }
